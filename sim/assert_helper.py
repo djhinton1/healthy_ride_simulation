929b6d38227c64fa8dee8717bc7bb5b56cbaced5
djhinton <54490164+djhinton1@users.noreply.github.com>
2020-11-29 13:07:10 -0600

    addd helpers for rider type and bike type

--- sim/assert_helper.py
+++ sim/assert_helper.py
@@ -24,6 +24,28 @@ def assert_id(id):
     id: [int] Number that must be type int
     """
     if not isinstance(id, int):
-        raise TypeError("id must be in int")
+        raise TypeError("id must be in int.")
     else:
         assert_greater_than_zero(id, "id")
+
+def assert_bike_type(type):
+    """
+    Raises Type Error if the bike type is not manual or electric.
+
+    Params
+    --------
+    type: [str] Type of bike.
+    """
+    if type not in ("electric", "namual"):
+        raise ValueError(f"{type} is not a valid bike type.")
+
+def assert_rider(rider):
+    """
+    Raises value error if rider is not an undergraduate, graduate, or faculty
+
+    Params
+    --------
+    rider: [str] The type of bike rider.
+    """
+    if rider not in ("undergraduate", "graduate", "faculty"):
+        raise ValueError(f"{rider} is not a valid bike rider.")
