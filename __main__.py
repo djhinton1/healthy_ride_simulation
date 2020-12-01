322f866d18a24e556c161b5c31d29cda8f3dc0ac
djhinton <54490164+djhinton1@users.noreply.github.com>
2020-12-01 00:17:55 -0600

    initial commit

---
+++ __main__.py
@@ -0,0 +1,8 @@
+from sim.dock import Dock
+from sim.bike import Bike
+from sim.station import Station
+from sim.sim import Simulation
+
+if __name__ == '__main__':
+    
+    Simulation('~/Documents/GitHub/healthy_ride_simulation/sim/stations.xlsx')
